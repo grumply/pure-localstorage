db8a0c36330e8ca6c094254088ff1d021310ac50
Sean Hickman <sean@grumply.com>
2020-02-13 23:33:04 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "pure-localstorage";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [
     base pure-json pure-lifted pure-txt unordered-containers
